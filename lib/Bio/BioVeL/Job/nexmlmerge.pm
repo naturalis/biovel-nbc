dca18d3c1c8d8173f917ec314861c19e2e1963f5
rvosa <rutgeraldo@gmail.com>
2014-01-24 12:29:28 +0100

    created a separate getter for the executable name (by default this is just the NAME query string param). Added the names of the arguments that need to become infiles.

--- lib/Bio/BioVeL/Job/nexmlmerge.pm
+++ lib/Bio/BioVeL/Job/nexmlmerge.pm
@@ -2,6 +2,8 @@ package Bio::BioVeL::Job::nexmlmerge;
 use Bio::BioVeL::Job;
 use base 'Bio::BioVeL::Job';
 
-sub name { 'biovel_nexmlmerge.pl' }
+sub exe { 'biovel_nexmlmerge.pl' }
+
+sub fileparams { qw[tree data meta] }
 
 1;
