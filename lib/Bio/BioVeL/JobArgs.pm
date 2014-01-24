a9835e79d8b5078b86f582b16d68572a27d30dfb
rvosa <rutgeraldo@gmail.com>
2014-01-24 13:31:40 +0100

    adding command object

---
+++ lib/Bio/BioVeL/JobArgs.pm
@@ -0,0 +1,35 @@
+package Bio::BioVeL::JobArgs;
+use strict;
+use warnings;
+use JSON;
+use MIME::Base64;
+use LWP::UserAgent;
+use File::Temp qw[tempfile];
+
+sub new {
+	my ( $class, $argstring, $job ) = @_;
+	
+	# decode the arguments
+	my $args_json_string = decode_base64($argstring);
+	my $args_hash_ref    = decode_json($args_json_string);
+
+	# download the input files from their URLs
+	my %files;
+	my $ua = LWP::UserAgent->new;
+	for my $param ( $job->fileparams ) {
+		my $url = delete $args_hash_ref->{$param};
+		my $response = $ua->get($url);
+		if ( $response->is_success ) {
+			my ( $fh, $filename ) = tempfile( 'DIR' => $job->jobdir );
+			print $fh $response->decoded_content;
+			$files{$param} = $filename;
+		}
+	}
+	return bless { 'files' => \%files, 'args' => $args_hash_ref }, $class;
+}
+
+sub files { shift->{'files'} }
+
+sub args { shift->{'args'} }
+
+1;
