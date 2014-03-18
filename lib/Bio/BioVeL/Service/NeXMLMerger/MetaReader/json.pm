418ab1f9535fc4a3a838325c4e3e0768ef635cfe
Hannes Hettling <hannes.hettling@gmail.com>
2014-03-18 17:48:53 +0100

    added documentation

--- lib/Bio/BioVeL/Service/NeXMLMerger/MetaReader/json.pm
+++ lib/Bio/BioVeL/Service/NeXMLMerger/MetaReader/json.pm
@@ -2,7 +2,14 @@ package Bio::BioVeL::Service::NeXMLMerger::MetaReader::json;
 use base Bio::BioVeL::Service::NeXMLMerger::MetaReader;
 use JSON;
 
+=item read_meta
 
+Function to read meta data from a json file handle. Returns array
+of hashes with key/value pairs representing metadata for 
+taxa.
+
+=cut
+ 
 sub read_meta {
     my ($self, $fh) = @_;
     my @result = ();
@@ -21,5 +28,3 @@ sub read_meta {
 }
 
 1;
-
-
