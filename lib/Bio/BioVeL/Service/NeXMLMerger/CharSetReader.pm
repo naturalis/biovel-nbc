2356b70e0846b6a775e4883a5466db7733302976
Rutger Vos <rutgeraldo@gmail.com>
2014-03-18 12:09:14 +0100

    readers now inherit from Reader.pm

--- lib/Bio/BioVeL/Service/NeXMLMerger/CharSetReader.pm
+++ lib/Bio/BioVeL/Service/NeXMLMerger/CharSetReader.pm
@@ -1,4 +1,9 @@
 package Bio::BioVeL::Service::NeXMLMerger::CharSetReader;
+use strict;
+use warnings;
+use Bio::BioVeL::Service::NeXMLMerger::Reader;
+use base 'Bio::BioVeL::Service::NeXMLMerger::Reader';
+
 
 sub read_charset {
   die "Implement me!";
--- lib/Bio/BioVeL/Service/NeXMLMerger/TreeReader.pm
+++ lib/Bio/BioVeL/Service/NeXMLMerger/TreeReader.pm
@@ -1,4 +1,8 @@
 package Bio::BioVeL::Service::NeXMLMerger::TreeReader;
+use strict;
+use warnings;
+use Bio::BioVeL::Service::NeXMLMerger::Reader;
+use base 'Bio::BioVeL::Service::NeXMLMerger::Reader';
 
 sub read_tree {
   die "Implement me!";
