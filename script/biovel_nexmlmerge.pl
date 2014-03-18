107adfb8ff645f450c6b63e277fc33c4ee9141d3
Rutger Vos <rutgeraldo@gmail.com>
2014-03-18 19:49:07 +0100

    removing old cruft

--- script/biovel_nexmlmerge.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-use Bio::BioVeL::Service::NeXMLMerger;
-my $merger = Bio::BioVeL::Service::NeXMLMerger->new;
-print $merger->response_header;
-print $merger->response_body;
