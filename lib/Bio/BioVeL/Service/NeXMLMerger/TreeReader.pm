dfbf24611e439eabf6c0ae0719c28bac3e36cf64
Rutger Vos <rutgeraldo@gmail.com>
2014-03-18 13:21:28 +0100

    actually don't need child classes to do anything

--- lib/Bio/BioVeL/Service/NeXMLMerger/TreeReader.pm
+++ lib/Bio/BioVeL/Service/NeXMLMerger/TreeReader.pm
@@ -1,23 +1,33 @@
 package Bio::BioVeL::Service::NeXMLMerger::TreeReader;
 use strict;
 use warnings;
+use Bio::Phylo::IO 'parse';
+use Bio::Phylo::Util::CONSTANT ':objecttypes';
 use Bio::BioVeL::Service::NeXMLMerger::Reader;
 use base 'Bio::BioVeL::Service::NeXMLMerger::Reader';
 
 =over
 
-=item read_tree
+=item read_trees
 
-This abstract method, which is implemented by the child classes, is passed a readable
+This method, which may or may not be overrided by the child classes, is passed a readable
 handle from which it reads a list of L<Bio::Phylo::Forest::Tree> objects.
 
 =back
 
 =cut
 
-sub read_tree {
+sub read_trees {
 	my ( $self, $handle ) = @_;
-	die "Implement me!";
+	my $format = ref($self);
+	$format =~ s/.+://;
+	return @{
+		parse(
+			'-format' => $format,
+			'-handle' => $handle,
+			'-as_project' => 1,	
+		)->get_items(_TREE_)
+	};
 }
 
 1;
