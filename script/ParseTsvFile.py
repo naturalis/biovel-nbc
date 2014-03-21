a5669654ec53e81155345443c3186966b652e633
Bach <bach@Bach-PC.(none)>
2014-03-21 13:32:18 +0100

    Comments inserted

--- script/ParseTsvFile.py
+++ script/ParseTsvFile.py
@@ -1,15 +1,15 @@
 import sys,os
 
-sys.stdout.write('Insert the input file location\n')
+sys.stdout.write('Insert the input file location\n') # Metadata TSV file location. the first column is often TaxonID
 Input=raw_input()
 
-sys.stdout.write('Insert a list of column names seperated by semicolon\n')
+sys.stdout.write('Insert a list of column names seperated by semicolon\n') # list of headers names that you wish to extract and put them in csv file (e.g. 'GeographicalOrigin;Pathogenic_Potential;Biomass;host;phenology' OR 'Biomass')
+
 Cols=raw_input()
 
-S='GeographicalOrigin;Pathogenic_Potential;Biomass;host;phenology'
-S1='Biomass'
-
 def dict2TSV(D_TaxRequestedValues):
+	'''A dictionary having as keys TaxonIDs and as values 
+	a list of the values of the requested columns'''
 	L=[]
 	for TaxID in D_TaxRequestedValues:
 		S_KeyValue=TaxID+','+','.join(D_TaxRequestedValues[TaxID])
@@ -18,7 +18,10 @@ def dict2TSV(D_TaxRequestedValues):
 
 def getColsOfInterest(f_in, colNames):
 	'''f_in is an input tsv file with headers
-	colNames should be a commaSeparated string of headerNames'''
+	colNames should be a semicolon separated string of headerNames.
+	This function outputs a file compatible with ITOL display tools for single
+	and multiple value bar charts, where the first line starts with the word 'LABELS'
+	and the rest of the file is a comma-separated values.'''
 	ColOfInterest={}
 	f=open(f_in, 'r')
 	Headers=[]
@@ -48,7 +51,7 @@ def getColsOfInterest(f_in, colNames):
 				ColOfInterest.setdefault(Taxon,[]).append(AllCols[j])
 	f.close()
 	f_out=open(f_in + '_ColsOfInterest.csv','w')
-	f_out.write('LABELS,'+ ','.join(colNames.split(';'))+ '\n')
+	f_out.write('LABELS,'+ ','.join(colNames.split(';')) + '\n')
 	Cols2Write=dict2TSV(ColOfInterest)
 	f_out.write('\n'.join(Cols2Write)+ '\n')
 	f_out.close()
