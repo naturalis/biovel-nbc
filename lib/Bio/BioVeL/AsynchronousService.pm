6eb565cfebe36cc9a46f4d2feb3e192326d318d9
hettling <hannes.hettling@gmail.com>
2014-07-10 15:04:23 +0200

    typo

--- lib/Bio/BioVeL/AsynchronousService.pm
+++ lib/Bio/BioVeL/AsynchronousService.pm
@@ -281,7 +281,6 @@ sub handler {
 		'request' => $request, 
 		'jobid'   => ( $request->param('jobid') || 0 ),
 	);
-	$self->logger->info("USER: ".$ENV{'USER'});
 	
 	if ( $self->status eq DONE ) {
 		$self->logger->info("asynchronous server status: DONE. response location: ".$self->response_location);
